9d6f5fb7f809eebcff0298d0da8f05a98407e6e9
unknown <giorgosmavroudis@gmail.com>
2020-03-23 02:18:12 +0100

    Updates

---
+++ precache-manifest.9fd9d62b4403022b05f3d5e670c8ca02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa9c965094020aa92d4e5b8507a75c1",
+    "url": "/test-case/index.html"
+  },
+  {
+    "revision": "0e57945ddf14b93adac1",
+    "url": "/test-case/static/css/main.487c651c.chunk.css"
+  },
+  {
+    "revision": "870ab4a74b474b6e93a1",
+    "url": "/test-case/static/js/2.16c96619.chunk.js"
+  },
+  {
+    "revision": "ae5f497210763b7678bafd2c5e80faba",
+    "url": "/test-case/static/js/2.16c96619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e57945ddf14b93adac1",
+    "url": "/test-case/static/js/main.99d1d355.chunk.js"
+  },
+  {
+    "revision": "d12f62bacf79211869db",
+    "url": "/test-case/static/js/runtime-main.1c3bb223.js"
+  }
+]);
